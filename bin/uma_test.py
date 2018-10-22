b4b939651e4d146b89adc622f93f95c0e3cd18b7
siqihuang <siqihuang2014@gmail.com>
2018-10-22 04:09:05 -0400

    update functional test framework and add test cases

--- bin/uma_test.py
+++ bin/uma_test.py
@@ -6,9 +6,11 @@ import errno
 import importlib
 import traceback
 import subprocess
-from subprocess import call
 from cluster.cluster import YamlManager
 from cluster.cluster_setting import *
+from client.UMARest import UMAClientWorld
+from client.UMARest import UMARestService
+from qa.util.util import *
 
 current_dir = os.getcwd()
 uma_core_test_bin = os.path.join(UMA_SIM_HOME, 'suites', 'test', 'functional', 'playground', 'bin')
@@ -22,19 +24,6 @@ def copy_uma_binary(uma_bin_path):
             shutil.copy(uma_bin_path, uma_core_test_bin)
         else:
             raise
-
-def start_uma():
-    os.chdir(uma_core_test_bin)
-    cmd = ["UMA.exe", 'start']
-    p = call(cmd)
-    time.sleep(3)
-    os.chdir(current_dir)
-
-def stop_uma():
-    os.chdir(uma_core_test_bin)
-    cmd = ["UMA.exe", 'stop']
-    call(cmd)
-    os.chdir(current_dir)
 
 def remove_old_bin():
     old_bin_path = os.path.join(UMA_SIM_HOME, 'suites', 'test', 'functional', 'playground', 'bin')
@@ -56,29 +45,37 @@ def test_yml_precheck(data):
 
 def launch_test(data):
     script = importlib.import_module(data['tests']['script'])
-    func = getattr(script, data['tests']['name'])
-    func()
+    for func_name in data['tests']['name']:
+        func = getattr(script, func_name)
+        func()
+        print "test passed for %s" %func_name
+        time.sleep(2)
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print "To run a functional test: uma_test.py <test.yml>"
         exit()
 
+    if 'UMA_HOME' not in os.environ:
+        print "UMA_HOME is not set, please go to UMA Core and run setEnv or Manually set UMA_HOME"
+        exit()
+
+    UMA_HOME = os.environ['UMA_HOME']
+    uma_bin_path = os.path.join(UMA_HOME, 'bin')
     test_yml = sys.argv[1]
     data = YamlManager(test_yml).get_dict()
     test_yml_precheck(data)
 
     remove_old_bin()
-    copy_uma_binary(data['uma_bin_path'])
+    copy_uma_binary(uma_bin_path)
 
     try:
-        start_uma()
         launch_test(data)
         print "Test succeed!"
     except Exception as ex:
         tb = traceback.format_exc()
         print tb
         print "Test Fail!"
-    finally:
+    finally: #just incase any uma is running
         stop_uma()
 
--- lib/qa/util/util.py
+++ lib/qa/util/util.py
@@ -1,7 +1,31 @@
 import sys
 import os
 import time
+from subprocess import call
 from ConfigParser import SafeConfigParser
+from cluster.cluster_setting import *
+import platform
+
+current_dir = os.getcwd()
+uma_core_test_bin = os.path.join(UMA_SIM_HOME, 'suites', 'test', 'functional', 'playground', 'bin')
+binary_name = "UMA.exe" if 'Windows' == platform.system() else "./UMA"
+
+
+# start UMA
+def start_uma():
+    os.chdir(uma_core_test_bin)
+
+    cmd = [binary_name, 'start']
+    p = call(cmd)
+    time.sleep(3)
+    os.chdir(current_dir)
+
+# stop UMA
+def stop_uma():
+    os.chdir(uma_core_test_bin)
+    cmd = [binary_name, 'stop']
+    call(cmd)
+    os.chdir(current_dir)
 
 # update the conf file
 def update_ini(ini_path, stanza, kwargs):
